821698ac84f60cf6eef138ac9f0b0b4ef01d5627
anantkaushik <anant.kaushik2@gmail.com>
2019-02-03 18:39:15 +0530

    Chandu's Gangsters

---
+++ C/10-Chandu's-Gangsters.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a[9],i,target,sum=0;
+    for(i=0;i<9;i++){
+        scanf("%d",&a[i]); // Taking Input
+        sum += a[i]; // Calculating Sum of Array
+    }
+    target = sum - 100; // Calculating the target which makes the sum of array greater than 100
+    int extraA,extraB; // Variables to store two numbers which makes the sum of array greater than 100
+    for(i=0;i<8;i++){
+        for(int j=i+1;j<9;j++){
+            if(a[i] + a[j] == target){
+                extraA = a[i];
+                extraB = a[j];
+                break;
+            }
+        }
+    }
+    for(i=0;i<9;i++){
+        if(a[i]!=extraA && a[i]!=extraB)
+            printf("%d ",a[i]); // Printing the result
+    }
+}
