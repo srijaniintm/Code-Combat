91faa2dce5942cb0431874654e3f2ec45a885845
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-02-03 23:04:37 +0530

    Encoded Result
    Updated and added test case loop

--- C/08-Encoded-Result.c
+++ C/08-Encoded-Result.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    char string[10000];
-    int count;
-    long long int prod = 1;
-    
-    scanf("%s", string);        // intaking string input
-    
-    for (count = 0; string[count]!='\0'; count++)         //traversing the string
-    {
-        if ((string[count] >= '0') && (string[count] <= '9'))     //checking for digit
+    int t;
+    scanf("%d",&t);
+    while (t>0){
+        char string[10000];
+        int count;
+        long long int prod = 1;
+        
+        scanf("%s", string);        // intaking string input
+        
+        for (count = 0; string[count]!='\0'; count++)         //traversing the string
         {
-            prod *= (string[count] - '0');          //multiplying the digits
+            if ((string[count] >= '0') && (string[count] <= '9'))     //checking for digit
+            {
+                prod *= (string[count] - '0');          //multiplying the digits
+            }
         }
+        printf("%lld\n", prod);
+        t--;
     }
-    printf("%lld", prod);
     return 0;
 }
