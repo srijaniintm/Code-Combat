125527181f100ef512225bbc2971e6e261ac46e8
HarshitDahiya <35694518+HarshitDahiya@users.noreply.github.com>
2019-02-03 23:25:31 +0530

    Word Without Vowels

---
+++ C++/04-Word-without-Vowels.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+#include <stdio.h>
+int main(){
+    char s[100];
+    int i=0;
+    cin>>s; //Taking Input String
+    while(s[i]!='\0'){
+        if(s[i] != 'a' && s[i] != 'A' && s[i] != 'e' && s[i] != 'E' && s[i] != 'i' && s[i] != 'I' && s[i] != 'o' && s[i] != 'O' && s[i] != 'u' && s[i] != 'U') //Checking for vowels
+            cout<<s[i]; //Displaying output
+        i++;
+    }
+    return 0;
+}
